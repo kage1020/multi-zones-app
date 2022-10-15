4c27e83061b62957b145be466a45481c8ddfe195
kage1020 <4620087@ed.tus.ac.jp>
2022-10-16 00:36:37 +0900

    Add multiple Zones config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,41 @@
+const { BLOG_URL, CHAT_URL, EC_URL } = process.env;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/blog',
+        destination: `${BLOG_URL}/blog`,
+      },
+      {
+        source: '/blog/:path*',
+        destination: `${BLOG_URL}/blog/:path*`,
+      },
+      {
+        source: '/chat',
+        destination: `${CHAT_URL}/chat`
+      },
+      {
+        source: '/chat/:path*',
+        destination: `${CHAT_URL}/chat/:path*`
+      },
+      {
+        source: '/ec',
+        destination: `${EC_URL}/ec`
+      },
+      {
+        source: '/ec/:path*',
+        destination: `${EC_URL}/ec/:path*`
+      }
+    ];
+  },
+};
 
 module.exports = nextConfig
